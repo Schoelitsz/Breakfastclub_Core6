a3980b7c62a9aac18255a403155a7f7efbb2e7c8
Terrinia <86646827+Schoelitsz@users.noreply.github.com>
2023-10-04 13:06:43 +0200

    styling changes

---
+++ BreakfastClub/Controllers/BoothController.cs
@@ -0,0 +1,59 @@
+﻿using BreakfastClub.Data;
+using BreakfastClub.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BreakfastClub.Controllers
+{
+    public class BoothController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public BoothController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            IEnumerable<Booth> objBoothList = _context.Booths;
+            return View(objBoothList);
+        
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();  
+        }
+
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        public IActionResult Update()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+
+        public IActionResult Assign()
+        {
+            return View();
+        }
+        
+    }
+}
